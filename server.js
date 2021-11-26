223231c5626691d1eea8600466ef2f7c9d02797f
Nikhil Mehta <nikhilmehta1501@gmail.com>
2021-11-26 19:42:10 +0530

    Static files serving syntax fix

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.use('/api/auth', authRoutes)
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
 //Start server
