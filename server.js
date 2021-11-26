474b8361389c2488e2bda32981c1646c6ba83b20
Nikhil Mehta <nikhilmehta1501@gmail.com>
2021-11-26 20:27:04 +0530

    Static file serving syntax fixed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const connectDB = require('./config/db')
 const app = express()
 
 //Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 //Load config
 if (process.env.NODE_ENV !== "production") {
